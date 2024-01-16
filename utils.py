4a3e5cdb94968c6e5279abadaa0b26be4cdbc474
Korn <kornsook2@gmail.com>
2024-01-16 15:13:11 -0600

    add

--- utils.py
+++ utils.py
@@ -3,7 +3,8 @@ import tensorflow as tf
 
 def torch_model(tf_model):
     def get_output(torch_inp):
+        torch_inp = torch.inp.transpose(0,2,3,1)
         np_inp = torch_inp.numpy()
         tf_inp = tf.convert_to_tensor(np_inp)
-        return tf_model.predict(tf_inp)
+        return torch.FloatTensor(tf_model.predict(tf_inp).numpy())
     return get_output
