24c000a09c3981cdbd5acf3587c4f42bcb9123f3
Korn <kornsook2@gmail.com>
2024-03-21 16:58:38 -0500

    add tinyimagenet

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import torch
 import tensorflow as tf
+import numpy as np
 from torch import Tensor as t
 
 def torch_model(tf_model):
@@ -9,3 +10,9 @@ def torch_model(tf_model):
         tf_inp = tf.convert_to_tensor(np_inp)
         return t(tf_model.predict(tf_inp, verbose = 0))
     return get_output
+
+def PILListToNumpy(lst, h, w, c):
+    out = np.zeros((len(lst), h, w, c))
+    for i, obj in enumerate(lst):
+        out[i] = np.asarray(obj.getdata()).reshape(h, w ,c)
+    return out
